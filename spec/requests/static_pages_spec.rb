55b52b600a7bd5ad3f013e852e48dab9155abe72
Zed <zed@Macintosh.local>
2013-05-29 01:44:35 -0700

    updated spec tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,35 +1,60 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
   
   describe "Home page" do
     before { visit root_path}
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { '' }
+    
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
-    before { visit help_path}
+    before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
     
-    it { should have_content('Help')}
+    it_should_behave_like "all static pages"
     it { should have_title(full_title('Help'))}
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content('About') }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
+    it_should_behave_like "all static pages"
     it { should have_title(full_title("About Us")) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
-
-    it { should have_content('Contact') }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "all static pages"
     it { should have_title(full_title('Contact')) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title(''))
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,6 +8,6 @@ describe "User pages" do
     before{ visit signup_path }
 
     it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up'))}
+    it { should have_title(full_title('Sign Up'))}
   end
 end
