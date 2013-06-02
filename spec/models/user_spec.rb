c29a2ffd8b8d6af1c001d2c08e7e366b34a19a2d
Zed <zed@Macintosh.local>
2013-06-01 17:13:23 -0700

    made changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
@@ -84,6 +94,11 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when password is too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid}
+  end
+
   describe "return value of authenticate method" do
     before { @user.save }
     let(:found_user) { User.find_by(email: @user.email) }
@@ -100,4 +115,3 @@ describe User do
     end
   end
 end
-
